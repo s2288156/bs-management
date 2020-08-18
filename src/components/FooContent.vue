501e1458fba9447cd7338236bd47738a3b80c6f3
wcy <s2288156@163.com>
2020-08-18 20:53:40 +0800

    init

---
+++ src/components/FooContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    this is foo home
+    <slot>default slot</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooContent'
+}
+</script>
---
+++ src/views/Foo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>This is an Foo page</h1>
+    <FooContent>aaa</FooContent>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import FooContent from '@/components/FooContent'
+
+export default {
+  name: 'Foo',
+  components: {
+    FooContent
+  }
+}
+</script>
