0eaf9c3b2efcfc8f01139f2746197210d762dd96
wcy <s2288156@163.com>
2020-12-22 14:38:27 +0800

    难看有login界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,33 @@
 <template>
-  <div>
+  <div class="login-container">
+    <el-row>
+      <el-col :span="9">
+        <div class="grid-content bg-transparent"></div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-transparent">
 
-    <el-container>
-      <el-header>Header</el-header>
-      <el-main>
-        <el-row>
-          <el-col :span="9">
-            <div class="grid-content bg-transparent"></div>
-          </el-col>
-          <el-col :span="6">
-            <div class="grid-content bg-transparent">
+          <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px"
+                   class="demo-ruleForm">
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+            </el-form-item>
 
-              <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px"
-                       class="demo-ruleForm">
-                <el-form-item label="用户名" prop="username">
-                  <el-input v-model="ruleForm.username" autocomplete="off"></el-input>
-                </el-form-item>
-                <el-form-item label="密码" prop="password">
-                  <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
-                </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+              <el-button @click="resetForm('ruleForm')">重置</el-button>
+            </el-form-item>
+          </el-form>
 
-                <el-form-item>
-                  <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
-                  <el-button @click="resetForm('ruleForm')">重置</el-button>
-                </el-form-item>
-              </el-form>
-
-            </div>
-          </el-col>
-          <el-col :span="9">
-            <div class="grid-content bg-transparent"></div>
-          </el-col>
-        </el-row>
-
-      </el-main>
-      <el-footer>Footer</el-footer>
-    </el-container>
-
+        </div>
+      </el-col>
+      <el-col :span="9">
+        <div class="grid-content bg-transparent"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -106,78 +97,27 @@ export default {
 }
 </script>
 
-<style>
-.el-header, .el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
+<style lang="scss">
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+
+  .bg-transparent {
+    background: Transparent;
+  }
+
+  .grid-content {
+    border-radius: 4px;
+    min-height: 36px;
+  }
 }
 
-.el-aside {
-  background-color: #D3DCE6;
-  color: #333;
-  text-align: center;
-  line-height: 200px;
-}
-
-.el-main {
-  background-color: #FFFFFF;
-  color: #333;
-  text-align: center;
-  line-height: 160px;
-}
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
-
-.el-row {
-  margin-bottom: 20px;
-
-&
-:last-child {
-  margin-bottom: 0;
-}
-
-}
-.el-col {
-  border-radius: 4px;
-}
-
-.bg-purple-dark {
-  background: #99a9bf;
-}
-
-.bg-purple {
-  background: #d3dce6;
-}
-
-.bg-transparent {
-  background: Transparent;
-}
-
-.bg-purple-light {
-  background: #D9ECFF;
-}
-
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
-}
 
 </style>
