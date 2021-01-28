1b716cc3cabe42890473e2d60361f5b4c18e1633
wcy <s2288156@163.com>
2021-01-28 16:45:29 +0800

    global css copy

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from './router'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import '@/styles/index.scss' // global css
 
 Vue.config.productionTip = false
-Vue.use(ElementUI);
+Vue.use(ElementUI)
 
 new Vue({
   router,
