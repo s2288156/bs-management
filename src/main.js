90b013c404846cf0785a73b99e606e5a73e84684
wcy <s2288156@163.com>
2021-01-29 14:25:03 +0800

    coding

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import '@/styles/index.scss' // global css
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Layout from '@/layout'
 
 Vue.use(VueRouter)
 
-// import Layout from '@/layout'
-
-const routes = [
-
+const constantRoutes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login/index'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: () => import('@/views/dashboard/index'),
+        meta: { title: 'Dashboard', icon: 'dashboard' }
+      }
+    ]
+  }
 ]
 
-const router = new VueRouter({
-  routes
+const createRouter = () => new VueRouter({
+  scrollBehavior: () => ({ y: 0 }),
+  router: constantRoutes
 })
+
+const router = createRouter()
+
+export function resetRouter() {
+  const newRouter = createRouter()
+  router.matcher = newRouter.matcher
+}
 
 export default router
