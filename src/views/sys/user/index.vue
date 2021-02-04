fbee0e52ab34eaffa5f399caebe5da6c86783e03
wcy <s2288156@163.com>
2021-02-04 10:59:40 +0800

    用户列表分页

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -8,29 +8,33 @@
       fit
       highlight-current-row
     >
-      <el-table-column type="index" :index="0" align="center" label="No." width="95" />
+      <el-table-column type="index" :index="1" align="center" label="No." width="95" />
       <el-table-column prop="username" label="Username" align="center" />
       <el-table-column prop="name" label="Name" align="center" />
-      <el-table-column prop="gender" label="Gender" align="center" />
+      <el-table-column prop="gender" label="Gender" width="110" align="center" />
       <el-table-column class-name="status-col" label="Status" width="110" align="center">
         <template v-slot="scope">
           <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+      <el-table-column align="center" prop="created_at" label="Display_time" width="250">
         <template v-slot="scope">
           <i class="el-icon-time" />
           <span>{{ scope.row.createTime }}</span>
         </template>
       </el-table-column>
     </el-table>
+
+    <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="fetchData" />
   </div>
 </template>
 
 <script>
 import { getList } from '@/api/user'
+import Pagination from '@/components/Pagination'
 
 export default {
+  components: { Pagination },
   filters: {
     statusFilter(status) {
       const statusMap = {
@@ -44,6 +48,7 @@ export default {
   data() {
     return {
       list: null,
+      total: 0,
       listLoading: true,
       listQuery: {
         page: 1,
@@ -60,6 +65,7 @@ export default {
       this.listLoading = true
       getList(this.listQuery).then(response => {
         this.list = response.data.records
+        this.total = response.data.total
         this.listLoading = false
       })
     }
