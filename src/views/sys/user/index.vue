ef173b5fb01758cd60c7834f4ee5eb7e16dc38f3
wcy <s2288156@163.com>
2021-02-04 14:30:46 +0800

    用户列表，Form dialog展示

--- src/views/sys/user/index.vue
+++ src/views/sys/user/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-container">
     <el-table
+      :key="tableKey"
       v-loading="listLoading"
       :data="list"
       element-loading-text="Loading"
@@ -17,7 +18,7 @@
           <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="250">
+      <el-table-column align="center" prop="created_at" label="Register Time" width="250">
         <template v-slot="scope">
           <i class="el-icon-time" />
           <span>{{ scope.row.createTime }}</span>
@@ -33,6 +34,33 @@
     </el-table>
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="fetchData" />
+
+    <el-dialog :title="dialogTitleMap[dialogStatus]" :visible.sync="dialogFormVisible">
+      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="100px">
+        <el-form-item label="Id" prop="id">
+          <span>{{ temp.id }}</span>
+        </el-form-item>
+        <el-form-item label="Username" prop="username">
+          <el-input v-model="temp.username" />
+        </el-form-item>
+        <el-form-item label="Name" prop="name">
+          <el-input v-model="temp.name" />
+        </el-form-item>
+        <el-form-item label="Gender">
+          <el-radio-group v-model="temp.gender">
+            <el-radio label="MALE" />
+            <el-radio label="FEMALE" />
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="Status">
+          <el-radio-group v-model="temp.status">
+            <el-radio label="ACTIVE" />
+            <el-radio label="DELETED" />
+            <el-radio label="FROZEN" />
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -41,6 +69,7 @@ import { getList } from '@/api/user'
 import Pagination from '@/components/Pagination'
 
 export default {
+  name: 'UserList',
   components: { Pagination },
   filters: {
     statusFilter(status) {
@@ -54,6 +83,7 @@ export default {
   },
   data() {
     return {
+      tableKey: 0,
       list: null,
       total: 0,
       listLoading: true,
@@ -61,6 +91,23 @@ export default {
         page: 1,
         limit: 20,
         sort: '+id'
+      },
+      temp: {
+        id: undefined,
+        name: undefined,
+        gender: undefined,
+        status: undefined,
+        username: undefined,
+        createTime: undefined
+      },
+      dialogStatus: '', // dialog状态
+      dialogFormVisible: false, // dialog默认不显示
+      dialogTitleMap: {
+        update: '编辑',
+        create: '新增'
+      },
+      rules: {
+        id: [{ required: true, message: 'id is required', trigger: 'change' }]
       }
     }
   },
@@ -77,7 +124,19 @@ export default {
       })
     },
     handleUpdate(row) {
-      console.log('handle update')
+      this.temp = Object.assign({}, row)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    updateData() {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          console.log('valid')
+        }
+      })
     }
   }
 }
