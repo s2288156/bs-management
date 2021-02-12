a6b9685e96093338592c2047cd66e89686fe5c76
wcy <s2288156@163.com>
2021-02-12 11:46:29 +0800

    role列表，按钮添加

--- src/views/sys/roles/index.vue
+++ src/views/sys/roles/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <div><span>Hello test Barrrrrrr</span></div>
+  <div class="app-container">
+    <div class="filter-container">
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        type="success"
+        icon="el-icon-refresh"
+        @click="fetchData"
+      >
+        {{ titleMap.reload }}
+      </el-button>
+      <el-button
+        class="filter-item"
+        style="margin-left: 10px;"
+        type="primary"
+        icon="el-icon-edit"
+        @click="handleAddRole"
+      >
+        {{ titleMap.add }}
+      </el-button>
+    </div>
+  </div>
 </template>
+
+<script>
+
+export default {
+  name: 'RoleList',
+  data() {
+    return {
+      titleMap: {
+        reload: '刷新',
+        add: '新增'
+      }
+    }
+  },
+  methods: {
+    fetchData() {
+      alert('fetch data')
+    },
+    handleAddRole() {
+      alert('add role')
+    }
+  }
+}
+</script>
